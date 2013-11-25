67ffddadb0a67df65f7c363f7f428dcf4625f236
changha <Chalk@Chalk-PC>
2013-11-25 23:59:36 +0900

    attributes and simple getter & setters.

--- Trombone/src/classes/Memo.java
+++ Trombone/src/classes/Memo.java
@@ -1,5 +1,34 @@
 package classes;
 
 public class Memo {
+	private int x;
+	private int y;
+	private int opacity;  // 1~100
+	private String content;
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getOpacity() {
+		return opacity;
+	}
+	public void setOpacity(int opacity) {
+		this.opacity = opacity;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}	
 
 }
--- Trombone/src/classes/MusicSheet.java
+++ Trombone/src/classes/MusicSheet.java
@@ -1,5 +1,31 @@
 package classes;
 
-public class MusicSheet {
+import classes.Note;
+import classes.Memo;
 
+public class MusicSheet {
+	private int beat;
+	private int pages;
+	private String name;
+	private Note[][] note;
+	private Memo[][] memo;
+	
+	public int getBeat() {
+		return beat;
+	}
+	public void setBeat(int beat) {
+		this.beat = beat;
+	}
+	public int getPages() {
+		return pages;
+	}
+	public void setPages(int pages) {
+		this.pages = pages;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 }
