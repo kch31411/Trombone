d178134a7ae8bbf7e14a3f0c1a77b23ccf4d71b3
changha <Chalk@Chalk-PC>
2013-12-02 01:14:05 +0900

    remove title bar & untrack bin folder

--- Trombone/src/com/example/trombone/MainUIActivity.java
+++ Trombone/src/com/example/trombone/MainUIActivity.java
@@ -9,6 +9,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import db.*;
@@ -24,6 +26,11 @@ public class MainUIActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);  
+	    //set up full screen
+	    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,   
+	                WindowManager.LayoutParams.FLAG_FULLSCREEN);  
 
 		setContentView(R.layout.activity_main_ui);
 
